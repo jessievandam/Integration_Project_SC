017ac8085b4cc9ab7033a8b190996f593d4b535a
JannekeBlok <39946611+JannekeBlok@users.noreply.github.com>
2019-05-17 11:28:07 +0200

    Updated Nonlinmod

--- Parameter Estimation/Simplified model/ParameterEstimation.m
+++ Parameter Estimation/Simplified model/ParameterEstimation.m
@@ -1,15 +1,15 @@
 b2_est = 0.0002;
-Order = [1 0 2];
+Order = [1 1 2];
 Parameters = [par.I2;par.m2;par.g;par.c2;b2_est];
 InitialStates = [-pi/2;0];
 
 
 m = idnlgrey('nonlinmod',Order,Parameters,InitialStates);
 
-m.Parameters(1).Fixed = false;%true;
-m.Parameters(2).Fixed = false;%true;
-m.Parameters(3).Fixed = false;%true;
-m.Parameters(4).Fixed = false;%true;
+m.Parameters(1).Fixed = true;
+m.Parameters(2).Fixed = true;
+m.Parameters(3).Fixed = true;
+m.Parameters(4).Fixed = true;
 m.Parameters(5).Fixed = false;%true;
 
 load('Data_real_simplifiedv3');
@@ -18,6 +18,6 @@ data = -data_real;
 %[min,index] = min(data);
 data = data(29:end);
 
-data_id = iddata(data,[],0.01);
+data_id = iddata(data,zeros(size(data)),0.01);
 
 m2 = nlgreyest(data_id,m);
--- Parameter Estimation/Simplified model/nonlinmod.m
+++ Parameter Estimation/Simplified model/nonlinmod.m
@@ -1,7 +1,8 @@
 function [dtheta,y] = nonlinmod(t,theta,u,I2,m2,g,c2,b2,varargin)
 
 dtheta(1) = theta(2);
-dtheta(2) = 1/I2*(-m2*g*c2*sin(theta(1))-b2*theta(2));
+dtheta(2) = -b2/(m2*c2^2)*theta(2)-g/(c2)*sin(theta(1));
+%dtheta(2) = 1/I2*(-m2*g*c2*sin(theta(1))-b2*theta(2));
 
 y = theta(1);
 end
