91221635beb9e24fac9a1ac9c92c1e829f54f4d7
jessievandam <jessievandam@hotmail.com>
2019-05-19 14:25:57 +0200

    opschonen

--- Sensor Calibration/SensorCalibrationTrue.m
+++ Sensor Calibration/SensorCalibrationTrue.m
@@ -8,7 +8,7 @@ load('theta1_3_v3.mat'); th1m3.v3 = theta1.data;
 load('theta1_3_v4.mat'); th1m3.v4 = theta1.data;
 load('theta1_3_v5.mat'); th1m3.v5 = theta1.data;
 
-offset_th1 = mean([mean(th1m3.v2),mean(th1m3.v3),mean(th1m3.v4),mean(th1m3.v5)]);
+offset_th1 = mean2([th1m3.v2,th1m3.v3,th1m3.v4,th1m3.v5]);
 
 %% Gain theta 1
 
@@ -18,7 +18,7 @@ load('theta1_1_v3.mat'); th1m1.v3 = theta1.data;
 load('theta1_1_v4.mat'); th1m1.v4 = theta1.data;
 load('theta1_1_v5.mat'); th1m1.v5 = theta1.data;
 
-gain_th1_pi = pi/(mean([mean(th1m1.v2),mean(th1m1.v3),mean(th1m1.v4),mean(th1m1.v5)])-offset_th1);
+gain_th1_pi = pi/(mean2([th1m1.v2,th1m1.v3,th1m1.v4,th1m1.v5])-offset_th1);
 
 %% Bias theta 2
 
@@ -29,8 +29,7 @@ load('theta2_3_v17.mat'); th2m3.v17 = theta2.data;
 load('theta2_3_v18.mat'); th2m3.v18 = theta2.data;
 load('theta2_3_v19.mat'); th2m3.v19 = theta2.data;
 
-offset_th2 = mean(mean([th2m3.v16,th2m3.v17,th2m3.v18,th2m3.v19]));
-
+offset_th2 = mean2([th2m3.v16,th2m3.v17,th2m3.v18,th2m3.v19]);
 
 %% Gain theta 2
 
@@ -40,5 +39,4 @@ load('theta2_1_v3.mat'); th2m1.v3 = theta2.data;
 load('theta2_1_v4.mat'); th2m1.v4 = theta2.data;
 load('theta2_1_v5.mat'); th2m1.v5 = theta2.data;
 
-
-gain_th2 = pi/(mean([mean(th2m1.v2),mean(th2m1.v3),mean(th2m1.v4),mean(th2m1.v5)])-offset_th2);
+gain_th2 = pi/(mean2([th2m1.v2,th2m1.v3,th2m1.v4,th2m1.v5])-offset_th2);
