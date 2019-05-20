3f0c88684edfba4d58961637af4e88658a7a183c
Janneke <jannekeblok@hotmail.nl>
2019-05-20 14:38:37 +0200

    Nonlinmod

--- Parameter Estimation/NonlinearModel_v2.m
+++ Parameter Estimation/NonlinearModel_v2.m
@@ -1,18 +1,18 @@
 function [dtheta, y] = NonlinearModel_v2(t,theta,u,P1,P2,P3,g1,g2,b1,b2,varargin)
 
-theta(2) = theta(2)-theta(1);
+theta(2,1) = theta(2,1)-theta(1,1);
 
-M = [P1+P2+2*P3*cos(theta(2)) P1+P3*cos(theta(2));
-    P1+P3*cos(theta(2)) P2];
-C = [b1-P3*theta(4)*sin(theta(2)) -P3*(theta(3)+theta(4))*sin(theta(2));
-    P3*theta(3)*sin(theta(2)) b2];
-G = [-g1*sin(theta(1))-g2*sin(theta(1)+theta(2));
-    -g2*sin(theta(1)+theta(2))];
+M = [P1+P2+2*P3*cos(theta(2,1)) P1+P3*cos(theta(2,1));
+    P1+P3*cos(theta(2,1)) P2];
+C = [b1-P3*theta(4,1)*sin(theta(2,1)) -P3*(theta(3,1)+theta(4,1))*sin(theta(2,1));
+    P3*theta(3,1)*sin(theta(2,1)) b2];
+G = [-g1*sin(theta(1,1))-g2*sin(theta(1,1)+theta(2,1));
+    -g2*sin(theta(1,1)+theta(2,1))];
 
-dtheta(1) = theta(3);
-dtheta(2) = theta(4);
-dtheta(3:4) = pinv(M)*(-C*[theta(3);theta(4)]-G+u);
+dtheta(1,1) = theta(3,1);
+dtheta(2,1) = theta(4,1);
+dtheta(3:4,1) = pinv(M)*(-C*[theta(3,1);theta(4,1)]-G+u);
 
-y = [theta(1); theta(2)];
+y = [theta(1,1); theta(2,1)];
 
 end
