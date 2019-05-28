2ff3d391299098351560135e1e92305b4752d6b6
jessievandam <jessievandam@hotmail.com>
2019-05-28 11:36:39 +0200

    double pend

--- Parameter Estimation/Least Squares Estimation/theta 1/MakeError_doublepend.m
+++ Parameter Estimation/Least Squares Estimation/theta 1/MakeError_doublepend.m
@@ -12,11 +12,11 @@ function [error,y0] = MakeError_doublepend(vec,par,par_min_single,data,input)
     c2 = par_min_single(3);
     b2 = par_min_single(4);
     
-    t0 = 0:.01:4.01; 
+    t0 = 0:.01:(length(data)-1)/100; 
     for i=1:length(t0)
         input_actual = input(i,1);
-        nonlinmodel = @(t,theta) NonlinearModel_v2(t,theta,[input_actual';zeros(size(input_actual'))],par,l1,l2,I1,I2,m1,m2,c1,c2,b1,b2,km);
-        [t,y] = ode45(nonlinmodel,[0 35],[data(1,1);data(2,1);(data(1,5)-data(1,1))/0.04;(data(2,5)-data(2,1))/0.04]);
+        nonlinmodel = @(t,theta) NonlinearModel_v2(t,theta,[input_actual;0],par,l1,l2,I1,I2,m1,m2,c1,c2,b1,b2,km);
+        [t,y] = ode45(nonlinmodel,[0 30],[data(1,1);data(2,1);(data(1,5)-data(1,1))/0.04;(data(2,5)-data(2,1))/0.04]);
         y0 = interp1(t,y,t0);
         y0 = y0(i,1);
         Y(i,:) = y0;
@@ -26,5 +26,5 @@ function [error,y0] = MakeError_doublepend(vec,par,par_min_single,data,input)
 %     [t,y] = ode45(nonlinmodel,[0 35],[data(1,1);data(2,1);(data(1,5)-data(1,1))/0.04;(data(2,5)-data(2,1))/0.04]);
 %     y0 = interp1(t,y,t0);
 %     y0 = y0(:,1);
-    error = data(:,1) - Y;
+    error = data(1,:) - Y';
 end
