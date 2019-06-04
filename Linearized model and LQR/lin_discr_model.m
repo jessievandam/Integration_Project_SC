509dcb944599039ef65d90f4f5c9454d97337467
jessievandam <jessievandam@hotmail.com>
2019-06-04 12:35:43 +0200

    lin

--- Linearized model and LQR/lin_discr_model.m
+++ Linearized model and LQR/lin_discr_model.m
@@ -33,20 +33,23 @@ C = [1 0 0 0;
 %% Discretization
 T = 0.01; % sample time
 
-[S1,J1] = jordan(A_eq1);
-A_eq1_d = S1*exp(J1*T)*inv(S1);
-A_eq1_d = real(A_eq1_d);
+% [S1,J1] = jordan(A_eq1);
+% A_eq1_d = S1*exp(J1*T)*inv(S1);
+% A_eq1_d = real(A_eq1_d);
+% 
+% [S2,J2] = jordan(A_eq2);
+% A_eq2_d = S2*exp(J2*T)*inv(S2);
+% A_eq2_d = real(A_eq2_d);
+% 
+% B_eq1_d = inv(A_eq1)*(A_eq1_d-eye(4))*B;
+% B_eq1_d = real(B_eq1_d);
+% 
+% B_eq2_d = inv(A_eq2)*(A_eq2_d-eye(4))*B;
+% B_eq2_d = real(B_eq2_d);
 
-[S2,J2] = jordan(A_eq2);
-A_eq2_d = S2*exp(J2*T)*inv(S2);
-A_eq2_d = real(A_eq2_d);
-
-B_eq1_d = inv(A_eq1)*(A_eq1_d-eye(4))*B;
-B_eq1_d = real(B_eq1_d);
-
-B_eq2_d = inv(A_eq2)*(A_eq2_d-eye(4))*B;
-B_eq2_d = real(B_eq2_d);
-
+A_eq1_d = A_eq1*T + eye(4);
+A_eq2_d = A_eq2*T + eye(4);
+B_d = B*T;
 C_d = C;
 
 %% LQR
@@ -55,6 +58,6 @@ Q2 = diag([1 1 1 1]);
 R1 = diag([1 1]);
 R2 = diag([1 1]);
 
-[K1,~,~] = lqr(A_eq1_d,B_eq1_d,Q1,R1); % optimal gain K1 for eq1
-[K2,~,~] = lqr(A_eq2_d,B_eq2_d,Q2,R2); % optimal gain K2 for eq2
+[K1,~,~] = lqr(A_eq1_d,B_d,Q1,R1); % optimal gain K1 for eq1
+[K2,~,~] = lqr(A_eq2_d,B_d,Q2,R2); % optimal gain K2 for eq2
 
