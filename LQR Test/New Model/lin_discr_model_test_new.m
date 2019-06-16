c4d3231d4742ca116a41a0fe9d597f37bdad6850
jessievandam <jessievandam@hotmail.com>
2019-06-16 09:41:46 +0200

    kljl

--- LQR Test/New Model/lin_discr_model_test_new.m
+++ LQR Test/New Model/lin_discr_model_test_new.m
@@ -1,49 +1,36 @@
 clear all; close all; clc;
 run('parameters_model.m');
+load('linmat.mat')
 Ts = 0.01;
 h = 0.01;
-
-%% Linearized, discretized state space model
-
-% equilibrium 1: [pi;0;0;0] DOWN DOWN
-[A_eq1, B_eq1, C_eq1, D_eq1] = dlinmod('NonlinearModelSimulink_2016b_eq1',Ts,[pi;0;0;0],0);
-
-% equilibrium 2: [0;0;0;0] UP UP
-[A_eq2, B_eq2, C_eq2, D_eq2] = dlinmod('NonlinearModelSimulink_2016b_eq2',Ts,[0;0;0;0],0);
-
-% equilibrium 3: [0;pi;0;0] UP DOWN
-[A_eq3, B_eq3, C_eq3, D_eq3] = dlinmod('NonlinearModelSimulink_2016b_eq3',Ts,[0;pi;0;0],0);
-
-% equilibrium 4: [pi;pi;0;0] DOWN UP
-[A_eq4, B_eq4, C_eq4, D_eq4] = dlinmod('NonlinearModelSimulink_2016b_eq4',Ts,[pi;pi;0;0],0);
 
 %% LQR Controller K
 % DOWN DOWN
 Q1 = diag([0.2 0.4 0 0]); 
 R1 = 1; 
-[K1,~,~] = dlqr(A_eq1,B_eq1,Q1,R1,zeros(4,1));
+[K1,~,~] = dlqr(linmat.A1,linmat.B1,Q1,R1,zeros(4,1));
 
 % UP UP
 Q2 = diag([200 1 1 0.1]); 
 R2 = 1; 
-[K2,~,~] = dlqr(A_eq2,B_eq2,Q2,R2,zeros(4,1));
+[K2,~,~] = dlqr(linmat.A2,linmat.B2,Q2,R2,zeros(4,1));
 
 % UP DOWN
 Q3 = diag([0.2 0.8 .01 .01]); 
 R3 = 1000; 
-[K3,~,~] = dlqr(A_eq3,B_eq3,Q3,R3,zeros(4,1));
+[K3,~,~] = dlqr(linmat.A3,linmat.B3,Q3,R3,zeros(4,1));
 
 % DOWN UP
 Q4 = diag([0.02 0.04 0 0]); 
 R4 = 1000; 
-[K4,~,~] = dlqr(A_eq4,B_eq4,Q4,R4,zeros(4,1));
+[K4,~,~] = dlqr(linmat.A4,linmat.B4,Q4,R4,zeros(4,1));
 
 %% LQI Controllers Klq and Ki
 C_new = [1 0 0 0; 1 1 0 0];
-sysd1 = ss(A_eq1,B_eq1,C_new,D_eq1,Ts);
-sysd2 = ss(A_eq2,B_eq2,C_new,D_eq2,Ts);
-sysd3 = ss(A_eq3,B_eq3,C_new,D_eq3,Ts);
-sysd4 = ss(A_eq4,B_eq4,C_new,D_eq4,Ts);
+sysd1 = ss(linmat.A1,linmat.B1,C_new,linmat.D1,Ts);
+sysd2 = ss(linmat.A2,linmat.B2,C_new,linmat.D2,Ts);
+sysd3 = ss(linmat.A3,linmat.B3,C_new,linmat.D3,Ts);
+sysd4 = ss(linmat.A4,linmat.B4,C_new,linmat.D4,Ts);
 
 % DOWN DOWN
 Q1_lqi = diag([2 1 1 0.1 1 1]); 
