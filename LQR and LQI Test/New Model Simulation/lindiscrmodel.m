9dfdef9af2a9d2b67db0f04ebffdf0fc8c17ebf4
jessievandam <jessievandam@hotmail.com>
2019-06-16 20:41:17 +0200

    jkljl

--- LQR and LQI Test/New Model Simulation/lindiscrmodel.m
+++ LQR and LQI Test/New Model Simulation/lindiscrmodel.m
@@ -7,35 +7,6 @@ h = 0.01;
 % eq1, downdown, [pi;0;0;0]
 % eq2, upup, [0;0;0;0]
 
-% xref
-frac = -1/(matrcomp.P1*matrcomp.P2-(matrcomp.P3)^2);
-
-A_eq1_part = frac*[matrcomp.g1*matrcomp.P2-matrcomp.g2*matrcomp.P3 -matrcomp.g2*matrcomp.P3 par.b1*matrcomp.P2 -par.b2*(matrcomp.P2+matrcomp.P3);
-             -matrcomp.g1*(matrcomp.P2+matrcomp.P3)+matrcomp.g2*(matrcomp.P1+matrcomp.P3) matrcomp.g2*(matrcomp.P1+matrcomp.P3) -par.b1*(matrcomp.P2+matrcomp.P3) par.b2*(matrcomp.P1+matrcomp.P2+2*matrcomp.P3)];
-
-A_eq1 = [0 0 1 0;
-        0 0 0 1;
-        A_eq1_part];
-
-B_part = frac*[-matrcomp.P2*par.km;
-         par.km*(matrcomp.P2+matrcomp.P3)];
-     
-B = [0 0; 
-    0 0; 
-    B_part zeros(2,1)];
-
-C = [1 0 0 0;
-    0 1 0 0];
-    
-x0_eq1 = -A_eq1*[pi;0;0;0];
-B_eq1 = [B x0_eq1];
-D_eq1 = zeros(2,3);
-
-sys1_val = ss(A_eq1,B_eq1,C,D_eq1);
-sysd1_val = c2d(sys1_val,Ts);
-B_eq1_d_val = sysd1_val.B;
-xref = B_eq1_d_val(:,3);
-
 %% Luenberger Observer 
 % DOWN DOWN
 p1 = pole(ss(linmat.A1,linmat.B1,linmat.C1,linmat.D1));
@@ -43,13 +14,11 @@ pdes1 = p1/3;
 L1 = place(linmat.A1',linmat.C1',pdes1)';
 eig_obs1 = eig(linmat.A1-L1*linmat.C1);
 
-% UP UP
-p2 = pole(ss(linmat.A2,linmat.B2,linmat.C2,linmat.D2));
-pdes2 = p2/3;
-L2 = place(linmat.A2',linmat.C2',pdes2)';
-eig_obs2 = eig(linmat.A2-L2*linmat.C2);
-
 %% LQR Controller K
+Q1 = diag([0.2 0.4 0 0]); 
+R1 = 1; 
+[K1,~,~] = dlqr(linmat.A1,linmat.B1,Q1,R1,zeros(4,1));
+
 Q2 = diag([300 1 1 0]); 
 R2 = 50; 
 [K2,~,~] = dlqr(linmat.A2,linmat.B2,Q2,R2,zeros(4,1));
@@ -58,7 +27,7 @@ R2 = 50;
 C_new = [1 0 0 0; 1 1 0 0];
 sysd2 = ss(linmat.A2,linmat.B2,C_new,linmat.D2,Ts);
 
-Q2_lqi = diag([3 2 0 0 0.1 0.1]); 
+Q2_lqi = diag([3 3 0 0 0.5 0.5]); 
 R2_lqi = 1; 
 
 [K2_lqi,~,~] = lqi(sysd2, Q2_lqi, R2_lqi, zeros(6,1));
---
+++ MPC/linearized_matrices2.m
@@ -0,0 +1,32 @@
+%% Linearized, discretized state space model
+Ts2 = 0.001;
+
+% equilibrium 1: [pi;0;0;0] DOWN DOWN
+[A_eq1, B_eq1, C_eq1, D_eq1] = dlinmod('NonlinearModelSimulink_2016b_eq1',Ts2,[pi;0;0;0],0);
+linmat.A1 = A_eq1;
+linmat.B1 = B_eq1;
+linmat.C1 = C_eq1;
+linmat.D1 = D_eq1;
+
+% equilibrium 2: [0;0;0;0] UP UP
+[A_eq2, B_eq2, C_eq2, D_eq2] = dlinmod('NonlinearModelSimulink_2016b_eq2',Ts2,[0;0;0;0],0);
+linmat.A2 = A_eq2;
+linmat.B2 = B_eq2;
+linmat.C2 = C_eq2;
+linmat.D2 = D_eq2;
+
+% equilibrium 3: [0;pi;0;0] UP DOWN
+[A_eq3, B_eq3, C_eq3, D_eq3] = dlinmod('NonlinearModelSimulink_2016b_eq3',Ts2,[0;pi;0;0],0);
+linmat.A3 = A_eq3;
+linmat.B3 = B_eq3;
+linmat.C3 = C_eq3;
+linmat.D3 = D_eq3;
+
+% equilibrium 4: [pi;pi;0;0] DOWN UP
+[A_eq4, B_eq4, C_eq4, D_eq4] = dlinmod('NonlinearModelSimulink_2016b_eq4',Ts2,[pi;pi;0;0],0);
+linmat.A4 = A_eq4;
+linmat.B4 = B_eq4;
+linmat.C4 = C_eq4;
+linmat.D4 = D_eq4;
+
+save('linmat.mat', 'linmat');
