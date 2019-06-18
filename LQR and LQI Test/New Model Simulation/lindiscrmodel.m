7ebaaafbce0f632c86153bab16f014198a7f9a19
jessievandam <jessievandam@hotmail.com>
2019-06-18 18:06:11 +0200

    jklj

--- LQR and LQI Test/New Model Simulation/lindiscrmodel.m
+++ LQR and LQI Test/New Model Simulation/lindiscrmodel.m
@@ -1,6 +1,6 @@
 clear all; close all; clc;
 run('parameters_model.m');
-load('linmat.mat')
+load('linmat_sim.mat')
 Ts = 0.01;
 h = 0.01;
 
@@ -18,7 +18,7 @@ eig_obs1 = eig(linmat.A1-L1*linmat.C1);
 Q1 = diag([0.2 0.4 0 0]); 
 R1 = 1; 
 [K1,~,~] = dlqr(linmat.A1,linmat.B1,Q1,R1,zeros(4,1));
-
+ 
 Q2 = diag([300 1 1 0]); 
 R2 = 50; 
 [K2,~,~] = dlqr(linmat.A2,linmat.B2,Q2,R2,zeros(4,1));
@@ -26,6 +26,9 @@ R2 = 50;
 %% LQI Controllers
 C_new = [1 0 0 0; 1 1 0 0];
 sysd2 = ss(linmat.A2,linmat.B2,C_new,linmat.D2,Ts);
+
+% Q2_lqi = diag([500 1 1 0 400 1]); 
+% R2_lqi = 100;
 
 Q2_lqi = diag([3 3 0 0 0.5 0.5]); 
 R2_lqi = 1; 
